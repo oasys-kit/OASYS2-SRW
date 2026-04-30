57416188bc7650f774182e5ad0b0360adc1ecaa7
lrebuffi <lrebuffi@anl.gov>
2026-04-30 18:45:09 -0500

    Added Example Workspaces

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except AttributeError:
     from setuptools import find_packages, setup
 
 NAME = 'OASYS2-SRW'
-VERSION = '1.0.13'
+VERSION = '1.0.14'
 ISRELEASED = True
 
 DESCRIPTION = 'SRW in OASYS2'
